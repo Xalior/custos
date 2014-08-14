4883c31ac1ff391addfe7023e5e494bc55d21917
D. Rimron-Soutter <d@occa-design.com>
2014-08-14 21:18:36 +0100

    Getter no longer kills us.
    -Dx

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ var cache = require('./lib/custos');
 
 cache.set('pages', '0','This is a long page', ['part1', 'part2', 'part3']);
 
-cache.set('pages', '1','This is a small page', 'part2');
+cache.set('pages', 'secondPages','This is a small page', 'part2');
+
+
+cache.set('pages', 1,'This is a third page', ['part2', 'part4']);
 
 cache.debug();
 
 console.log(cache.get('pages',0));
+console.log(cache.get('pages',1));
+console.log(cache.get('pages',2));
