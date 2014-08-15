6f6bd506ddbfa31fe03f48abda2ba1d16ddf9fd3
D. Rimron-Soutter <d@occa-design.com>
2014-08-15 15:19:00 +0100

    .del() works, microoptimise .get() and add {indexes} support.
    -Dx

--- index.js
+++ index.js
@@ -4,14 +4,17 @@
 var cache = require('./lib/custos');
 
 cache.set('pages', '0','This is a long page', ['part1', 'part2', 'part3']);
-
 cache.set('pages', 'secondPages','This is a small page', 'part2');
-
-
 cache.set('pages', 1,'This is a third page', ['part2', 'part4']);
+cache.set('pages', 2,'This is a FOURTH page', ['part5', 'part4']);
 
 cache.debug();
 
 console.log(cache.get('pages',0));
 console.log(cache.get('pages',1));
-console.log(cache.get('pages',2));
+console.log(cache.get('pages',99));
+
+cache.del('pages', 'secondPages');
+
+
+cache.debug();
